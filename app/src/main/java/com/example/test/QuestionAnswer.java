b1051c64e9422c1b260d5b76e491f7777e2fb3bf
Ashok Kumar R <ashok.rapak@gmail.com>
2024-11-21 20:35:15 +0530

    Quiz App

---
+++ app/src/main/java/com/example/test/QuestionAnswer.java
@@ -0,0 +1,24 @@
+package com.example.test;
+
+public class QuestionAnswer {
+    public static String[] question = {
+            "What is the capital of France?",
+            "Who is the creator of Android?",
+            "Which is the largest planet in our Solar System?",
+            "What is the boiling point of water?"
+    };
+
+    public static String[][] choices = {
+            {"Paris", "London", "Rome", "Berlin"},
+            {"Larry Page", "Sundar Pichai", "Andy Rubin", "Elon Musk"},
+            {"Earth", "Mars", "Jupiter", "Venus"},
+            {"50°C", "75°C", "100°C", "150°C"}
+    };
+
+    public static String[] correctAnswers = {
+            "Paris",
+            "Andy Rubin",
+            "Jupiter",
+            "100°C"
+    };
+}
